ed8d2308b424eb67b59684aaa816d3630ad688d1
xiupos <me@xiupos.net>
2023-04-01 12:46:55 +0900

    renewal

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    // Transform string to Date object
+    date: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+  }),
+});
+
+export const collections = { blog };

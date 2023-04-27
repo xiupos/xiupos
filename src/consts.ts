1d9b82698daaa4b205e3c9286d19d88ad0d0c4fa
xiupos <me@xiupos.net>
2023-04-27 23:07:26 +0900

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = 'Xiupos';
-export const SITE_DESCRIPTION = 'Welcome to xiupos\' website!';
+export const SITE_DESCRIPTION = 'Welcome to xiupoy website!';
 export const BLOG_TITLE = 'Xiupoy Blog';
-export const BLOG_DESCRIPTION = 'This is xiupos\' blog.';
+export const BLOG_DESCRIPTION = 'This is xiupoy blog.';

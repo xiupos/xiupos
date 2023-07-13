6a7d6ceead8a05516557950bb346506365a0bfd1
xiupos <me@xiupos.net>
2023-07-13 21:02:25 +0900

    add author

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 export const SITE_TITLE = 'Xiupos';
+export const SITE_AUTHOR = 'xiupos';
 export const SITE_DESCRIPTION = 'Welcome to xiupoy website!';
 export const BLOG_TITLE = 'Xiupoy Blog';
 export const BLOG_DESCRIPTION = 'This is xiupoy blog.';

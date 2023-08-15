e8fec54f6a355a7e24b181f63e5f91379d82dbf7
xiupos <me@xiupos.net>
2023-08-15 16:50:21 +0900

    change rss place

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { BLOG_TITLE, BLOG_DESCRIPTION } from '../consts';
+import { BLOG_TITLE, BLOG_DESCRIPTION } from '../../consts';
 
 export async function get(context: any) {
   const blog = await getCollection('blog');

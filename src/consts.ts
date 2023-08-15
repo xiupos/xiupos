8faf7c99027b31405d016f15a6103ff1e0a1e312
xiupos <me@xiupos.net>
2023-08-15 19:59:58 +0900

    make header links const

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@ export const SITE_AUTHOR = 'xiupos';
 export const SITE_DESCRIPTION = 'Welcome to xiupoy website!';
 export const BLOG_TITLE = 'Xiupoy Blog';
 export const BLOG_DESCRIPTION = 'This is xiupoy blog.';
+export const HEADER_LINKS = [{ name: 'xiupos.net', link: '/' }, { name: 'blog', link: '/blog/' },];

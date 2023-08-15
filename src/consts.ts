b3cb68ed274e9fe211ef123c8b52b3d92da10565
xiupos <me@xiupos.net>
2023-08-15 20:26:29 +0900

    make social links const

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,14 @@ export const SITE_AUTHOR = 'xiupos';
 export const SITE_DESCRIPTION = 'Welcome to xiupoy website!';
 export const BLOG_TITLE = 'Xiupoy Blog';
 export const BLOG_DESCRIPTION = 'This is xiupoy blog.';
-export const HEADER_LINKS = [{ name: 'xiupos.net', link: '/' }, { name: 'blog', link: '/blog/' },];
+
+export const HEADER_LINKS = [
+  { name: 'xiupos.net', link: '/' },
+  { name: 'blog', link: '/blog/' },
+];
+
+export const SOCIAL_LINKS = [
+  { link: 'https://github.com/xiupos', icon: 'github' },
+  { link: 'https://twitter.com/xiupos', icon: 'twitter' },
+  { link: 'https://mk.xiupos.net/@xiupos', icon: 'activitypub' },
+];

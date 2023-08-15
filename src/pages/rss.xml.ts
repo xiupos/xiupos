489f3193a1eba6a41dbe68449569a0a1887a1f53
xiupos <me@xiupos.net>
2023-08-15 16:15:24 +0900

    rename date to pubDate

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -10,7 +10,8 @@ export async function get(context: any) {
     site: context.site,
     items: blog.map((post) => ({
       title: post.data.title,
-      pubDate: post.data.date,
+      pubDate: post.data.pubDate,
+      // description: post.data.description,
       // Compute RSS link from post `slug`
       // This example assumes all posts are rendered as `/blog/[slug]` routes
       link: `/blog/${post.slug}/`,

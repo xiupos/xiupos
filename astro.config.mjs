490e4a6e1c27744202e079a4896f91d1baa9f92e
xiupos <me@xiupos.net>
2024-01-29 15:55:11 +0900

    update 6 files and delete 1 file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,45 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import remarkMath from 'remark-math';
+import remarkDirective from 'remark-directive';
 import rehypeMathjax from 'rehype-mathjax/chtml';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import { AllPackages } from 'mathjax-full/js/input/tex/AllPackages.js';
-
 // @ts-ignore
 import rehypeWrapAll from 'rehype-wrap-all';
+
+/**
+ * options for rehypeMathjax
+ * - add plugins
+ * - add macros
+ * - TODO: add sonoisa/XyJax-v3
+ */
+const rehypeMathjaxOptions = {
+  tex: {
+    packages: [...AllPackages, ...["physics", "boldsymbol"]],
+    macros:{
+      bm: ["{\\boldsymbol{#1}}", 1],
+      "~": ["{\\tilde{#1}}", 1],
+      "^": ["{\\hat{#1}}", 1],
+      "=": ["{\\bar{#1}}", 1],
+      ".": ["{\\dot{#1}}", 1],
+      "\"": ["{\\ddot{#1}}", 1],
+    },
+  },
+  chtml: {
+    fontURL: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2'
+  }
+};
+
+/**
+ * options for rehypeWrapAll
+ * - add wrappers for responsive design
+ */
+const rehypeWrapAllOptions = [
+  {selector: 'table', wrapper: 'div.responsive'},
+  {selector: 'img', wrapper: 'div.responsive'},
+  {selector: 'video', wrapper: 'div.responsive'},
+];
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,32 +57,12 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [
       remarkMath,
+      remarkDirective,
     ],
     rehypePlugins: [
-      [rehypeMathjax, {
-        tex: {
-          packages: [...AllPackages, ...["physics", "boldsymbol"]],
-          inlineMath: [['\\(', '\\)']],
-          displayMath: [['\\[', '\\]']],
-          macros:{
-            bm: ["{\\boldsymbol{#1}}", 1],
-            "~": ["{\\tilde{#1}}", 1],
-            "^": ["{\\hat{#1}}", 1],
-            "=": ["{\\bar{#1}}", 1],
-            ".": ["{\\dot{#1}}", 1],
-            "\"": ["{\\ddot{#1}}", 1],
-          },
-        },
-        chtml: {
-          fontURL: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2'
-        }
-      }],
+      [rehypeMathjax, rehypeMathjaxOptions],
+      [rehypeWrapAll, rehypeWrapAllOptions],
       rehypeHeadingIds,
-      [rehypeWrapAll, [
-        {selector: 'table', wrapper: 'div.responsive'},
-        {selector: 'img', wrapper: 'div.responsive'},
-        {selector: 'video', wrapper: 'div.responsive'},
-      ]],
     ],
     shikiConfig: {
       theme: 'one-dark-pro',

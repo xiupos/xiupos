1b2214636a4f43da50d67da03c192d1ac7ffcbd1
xiupos <me@xiupos.net>
2024-03-23 22:31:31 +0900

    create 1 file and update 2 files

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -1,27 +1,48 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { BLOG_TITLE, BLOG_DESCRIPTION } from '../../consts.js';
+import OtherPosts from "../../other-posts.json";
 
 /**
  * @param {any} context
  * @returns {Promise<Response>}
  */
 export async function GET(context) {
-  const blog = await getCollection('blog');
+  /**
+   * @param {any} a
+   * @param {any} b
+   * @returns {number}
+   */
+  const sortByDate = (a, b) => b.pubDate.valueOf() - a.pubDate.valueOf();
+
+  const BlogPosts = (await getCollection("blog"))
+    .filter((post) => !post.data.draft)
+    .map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.slug}/`,
+    }));
+
+  const posts = [
+    ...BlogPosts,
+    ...OtherPosts.map((post) => ({
+      ...post,
+      pubDate: new Date(post.pubDate),
+    })),
+  ];
+
   return rss({
     title: BLOG_TITLE,
     description: BLOG_DESCRIPTION,
     site: context.site,
-    items: blog
-      .filter((post) => !post.data.draft)
-      .sort((a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime())
+    items: posts.sort(sortByDate)
       .map((post) => ({
-        title: post.data.title,
-        pubDate: post.data.pubDate,
-        // description: post.data.description,
+        title: post.title,
+        pubDate: post.pubDate,
+        // description: post.description,
         // Compute RSS link from post `slug`
         // This example assumes all posts are rendered as `/blog/[slug]` routes
-        link: `/blog/${post.slug}/`,
+        link: post.link,
       })),
   });
 }

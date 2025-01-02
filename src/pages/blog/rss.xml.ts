dbbbd9458f3f0c2b9569a5949dd130803149a702
xiupos <me@xiupos.net>
2025-01-02 23:46:10 +0900

    update rss.xml.ts

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -7,20 +7,16 @@ export async function GET(context: any): Promise<Response> {
   const sortByDate = (a: any, b: any): number =>
     b.pubDate.valueOf() - a.pubDate.valueOf();
 
-  const BlogPosts = await Promise.all(
-    (
-      await getCollection("blog")
-    )
-      .filter((post) => !post.data.draft)
-      .map(async (post) => ({
-        title: post.data.title,
-        pubDate: post.data.pubDate,
-        // description: post.description,
-        // Compute RSS link from post `slug`
-        // This example assumes all posts are rendered as `/blog/[slug]` routes
-        link: `/blog/${post.slug}/`,
-      }))
-  );
+  const BlogPosts = (await getCollection("blog"))
+    .filter((post) => !post.data.draft)
+    .map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      // description: post.description,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/blog/${post.slug}/`,
+    }));
 
   // combine other posts with blog posts
   const posts = [

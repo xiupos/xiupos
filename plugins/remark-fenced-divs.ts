67954bf6236088942fcebc11bf32bc791c735b12
xiupos <me@xiupos.net>
2025-11-24 11:54:01 +0900

    update remark-fenced-divs.ts

--- plugins/remark-fenced-divs.ts
+++ plugins/remark-fenced-divs.ts
@@ -1,24 +1,21 @@
 // wip
-import type { RemarkPlugin } from '@astrojs/markdown-remark';
-import { h } from 'hastscript';
-import { visit } from 'unist-util-visit';
+import type { RemarkPlugin } from "@astrojs/markdown-remark";
+import { h } from "hastscript";
+import { visit } from "unist-util-visit";
 
-// @ts-ignore
-const remarkFencedDivs: RemarkPlugin<any[]> = () => tree => visit(tree, (node) => {
-  // @ts-ignore
-  if (node.type === 'containerDirective') {
-    // @ts-ignore
-    if (node.name === 'screen') {
-      // @ts-ignore
-      const data = node.data || (node.data = {});
-      const tagName = 'div';
+/** rewite "::: ~ :::" to div blocks */
+const remarkFencedDivs: RemarkPlugin = () => (tree) =>
+  visit(tree, (node) => {
+    if (node.type === "containerDirective") {
+      // :::screen ~ :::
+      if (node.name === "screen") {
+        const data = node.data || (node.data = {});
+        const tagName = "div";
 
-      data.hName = tagName;
-      data.hProperties = h(tagName, { class: 'screen' }).properties;
-    } else {
-      return;
+        data.hName = tagName;
+        data.hProperties = h(tagName, { class: "screen" }).properties;
+      }
     }
-  }
-});
+  });
 
 export default remarkFencedDivs;

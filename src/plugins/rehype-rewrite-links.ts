318a801c262346241fa12245ca4aa35840ee61c3
xiupos <me@xiupos.net>
2025-11-27 02:55:45 +0900

    update rehype-rewrite-links.ts

--- src/plugins/rehype-rewrite-links.ts
+++ src/plugins/rehype-rewrite-links.ts
@@ -2,7 +2,7 @@ import type { RehypePlugin } from "@astrojs/markdown-remark";
 import { visit } from "unist-util-visit";
 
 /** extensions for post files */
-const extensions = ["md", "mdx", "typ"];
+const extensions = ["ipynb.md", "md", "mdx", "typ"];
 
 /**
  * Remove the extension of the relative link and solve the hierarchy.
